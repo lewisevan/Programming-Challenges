84888cbaba62e0958983f15066bf0e6ce5ebcb57
lewisevan <36089251+lewisevan@users.noreply.github.com>
2020-09-09 14:49:50 -0500

    Create problem file

---
+++ src/Skiena_Revilla/Chapter_1/Problem_10189.java
@@ -0,0 +1,10 @@
+package Skiena_Revilla.Chapter_1;
+
+import java.io.BufferedInputStream;
+import java.util.Scanner;
+
+public class Problem_10189 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(new BufferedInputStream(System.in));
+    }
+}
