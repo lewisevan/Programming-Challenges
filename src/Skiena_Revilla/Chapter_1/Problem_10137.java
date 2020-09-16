4d37558d4a43f0d8626e4157b2b95d9445ceee6e
lewisevan <36089251+lewisevan@users.noreply.github.com>
2020-09-15 21:07:30 -0500

    Adds pseudo-code

--- src/Skiena_Revilla/Chapter_1/Problem_10137.java
+++ src/Skiena_Revilla/Chapter_1/Problem_10137.java
@@ -1,10 +1,28 @@
 package Skiena_Revilla.Chapter_1;
 
 import java.io.BufferedInputStream;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Problem_10137 {
     public static void main(String[] args) {
         Scanner in = new Scanner(new BufferedInputStream(System.in));
+        ArrayList<double[]> trips = new ArrayList(); // Each index holds an array containing the amounts each student paid during the trip
+
+        /* Input data */
+        int student_count = in.nextInt();
+        while (student_count != 0) {
+            double[] payments;
+            // Get student_count lines of data containing the prices each student paid
+                // Add each line to the payments array
+            // Add the payments array to the trips ArrayList
+        }
+
+        /* Process and output data */
+        // For each payments array in the trips ArrayList
+            // Calculate the average payment
+            // Search through the payments array and identify all payments above the average payment
+                // Find the difference between the payment amount and avg payment, and add this difference to exchange
+            // Print the total amount that must be exchanged
     }
 }
