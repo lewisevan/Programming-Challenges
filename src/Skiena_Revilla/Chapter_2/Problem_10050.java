99ddf57fb6ccafbb0cfa3a0cf1a31bb93c230305
lewisevan <36089251+lewisevan@users.noreply.github.com>
2020-10-31 20:34:13 -0500

    Adds input collection

--- src/Skiena_Revilla/Chapter_2/Problem_10050.java
+++ src/Skiena_Revilla/Chapter_2/Problem_10050.java
@@ -8,6 +8,29 @@ import java.util.Scanner;
 public class Problem_10050 {
     public static void main(String[] args) {
         Scanner in = new Scanner(new BufferedInputStream(System.in));
-        
+
+        // Number of test cases
+        int numberOfCases = in.nextInt();
+        in.nextLine();
+
+        for (int i = 0; i < numberOfCases; i++) {
+            // Number of days to simulate
+            int days = in.nextInt();
+            in.nextLine();
+
+            // Number of political parties to simulate
+            int numberOfParties = in.nextInt();
+            in.nextLine();
+
+            int [] hartals = new int[numberOfParties];
+
+            for (int j = 0; j < numberOfParties; j++) {
+                // Get the hartal parameter of each party
+                hartals[j] = in.nextInt();
+                in.nextLine();
+            }
+
+            // Calculate number of working days lost
+        }
     }
 }
