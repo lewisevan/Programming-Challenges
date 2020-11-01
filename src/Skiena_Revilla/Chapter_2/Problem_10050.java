11de0b8918bd80e9923b0687ff730249ef63b064
lewisevan <36089251+lewisevan@users.noreply.github.com>
2020-10-31 21:06:06 -0500

    Solves Problem #10050

--- src/Skiena_Revilla/Chapter_2/Problem_10050.java
+++ src/Skiena_Revilla/Chapter_2/Problem_10050.java
@@ -13,6 +13,9 @@ public class Problem_10050 {
         int numberOfCases = in.nextInt();
         in.nextLine();
 
+        // Stored results
+        int [] results = new int[numberOfCases];
+
         for (int i = 0; i < numberOfCases; i++) {
             // Number of days to simulate
             int days = in.nextInt();
@@ -22,15 +25,39 @@ public class Problem_10050 {
             int numberOfParties = in.nextInt();
             in.nextLine();
 
-            int [] hartals = new int[numberOfParties];
+            ArrayList<Integer> hartals = new ArrayList<>();
 
             for (int j = 0; j < numberOfParties; j++) {
-                // Get the hartal parameter of each party
-                hartals[j] = in.nextInt();
+                // Get the hartal parameter of each party. Only store unique parameters
+                int p = in.nextInt();
                 in.nextLine();
+
+                if (!hartals.contains(p)) {
+                    hartals.add(p);
+                }
             }
 
             // Calculate number of working days lost
+            int lostDays = 0;
+            for (int day = 1; day <= days; day++) {
+                // Skip Fridays and Saturdays
+                if (!(day % 7 == 0 || day % 7 == 6)) {
+                    // Check if at least one hartal takes place on this day
+                    for (int h = 0; h < hartals.size(); h++) {
+                        if (day % hartals.get(h) == 0) {
+                            lostDays++;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            // Store result
+            results[i] = lostDays;
+        }
+
+        for (int i = 0; i < numberOfCases; i++) {
+            System.out.println(results[i]);
         }
     }
 }
