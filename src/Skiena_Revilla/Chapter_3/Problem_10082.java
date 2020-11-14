ba003271856f64b15c5a31434c9ea31d0b21821f
lewisevan <36089251+lewisevan@users.noreply.github.com>
2020-11-13 23:35:29 -0600

    Solves 10082

--- src/Skiena_Revilla/Chapter_3/Problem_10082.java
+++ src/Skiena_Revilla/Chapter_3/Problem_10082.java
@@ -1,7 +1,37 @@
 package Skiena_Revilla.Chapter_3;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Problem_10082 {
     public static void main(String[] args) {
-        
+        String chars = "QWERTYUIOP[]\\ASDFGHJKL;'ZXCVBNM,./`1234567890-=";
+        ArrayList<String> lines = new ArrayList<>(); // Stores lines of input
+
+        Scanner scan = new Scanner(System.in);
+        while (scan.hasNextLine()) {
+            // Collect input
+            String line = scan.nextLine();
+
+            if (line.equals("")) {
+                break;
+            }
+
+            lines.add(line);
+        }
+
+        // Process data and print output
+        for (int i = 0; i < lines.size(); i++) {
+            String line = lines.get(i);
+            for (int j = 0; j < line.length(); j++) {
+                char c = line.charAt(j);
+                if (c == ' ' || c == 'Q' || c == 'A' || c == 'Z' || c == '\n') {
+                    System.out.print(c);
+                } else {
+                    System.out.print(chars.charAt(chars.indexOf(c) - 1));
+                }
+            }
+            System.out.println();
+        }
     }
 }
