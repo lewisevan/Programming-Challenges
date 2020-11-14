d283b74cfe68935dd67e31f94671ca6b66b88b65
lewisevan <36089251+lewisevan@users.noreply.github.com>
2020-11-13 23:58:28 -0600

    Update Problem_10252.java

--- src/Skiena_Revilla/Chapter_3/Problem_10252.java
+++ src/Skiena_Revilla/Chapter_3/Problem_10252.java
@@ -1,7 +1,41 @@
 package Skiena_Revilla.Chapter_3;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Problem_10252 {
     public static void main(String[] args) {
-        
+        String alphabet = "abcdefghijklmnopqrstuvwxyz";
+        ArrayList<int[]> words = new ArrayList<>();
+
+        Scanner scan = new Scanner(System.in);
+        while (scan.hasNextLine()) {
+            String line = scan.nextLine();
+
+            if (line.equals("")) {
+                break;
+            }
+
+            int[] word = new int[alphabet.length()]; // Create a 26 element array representing the number of occurrences of each letter in the word
+
+            for (int i = 0; i < line.length(); i++) {
+                word[alphabet.indexOf(line.charAt(i))] += 1;
+            }
+
+            words.add(word); // Store in the array list
+        }
+
+        // Process data and print output
+        for (int i = 0; i < words.size(); i += 2) {
+            int[] word1 = words.get(i);
+            int[] word2 = words.get(i + 1);
+
+            for (int j = 0; j < alphabet.length(); j++) {
+                for (int k = 0; k < Math.min(word1[j], word2[j]); k++) {
+                    System.out.print(alphabet.charAt(j));
+                }
+            }
+            System.out.println();
+        }
     }
 }
